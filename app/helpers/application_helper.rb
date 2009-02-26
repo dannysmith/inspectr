d1d5e6a151dc89bcecb0d7d680cd35f3c90f9b66
danny <danny@thescri.be>
2009-02-26 19:58:19 +0000

    Ability to add payments added

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,8 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  
+  #Helper to convert amt to pounds
+  def pounds(amt)
+    sprintf("£%0.2f", amt)
+  end
 end
